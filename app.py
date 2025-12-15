c4b354b4ecac52a44150b1a6d5310d9090424276
prasunivy <prasun2111@gmail.com>
2025-12-15 16:21:09 +0530

    Initial Streamlit app

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Sales & Stock App", layout="wide")
+
+st.title("Sales & Stock Statement App")
+
+st.write("Welcome! App is running successfully ✅")
